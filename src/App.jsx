485482d54ee3096b83e0e462d747518ea685c6df
sag09 <nihal.evil.royal@gmail.com>
2023-09-25 06:05:38 +0530

    nav import app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import styles from "./style";
-import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components';
+import {  Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components';
+import Navbar from './components/NavBar'
 
 
 
