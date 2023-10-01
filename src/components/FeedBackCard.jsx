e7e0c612e7f397d384c99f718a92a34e88d6a71c
sag09 <nihal.evil.royal@gmail.com>
2023-10-01 21:31:05 +0530

    testimonial box resize

--- src/components/FeedBackCard.jsx
+++ src/components/FeedBackCard.jsx
@@ -1,7 +1,7 @@
 import { quotes } from "../assets";
 
 const FeedbackCard = ({ content, name, title, img }) => (
-  <div className="flex justify-between flex-col px-10 py-12 rounded-[20px]  max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+  <div className="flex justify-between flex-col px-10 py-12 rounded-[20px]  max-w-[400px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
     <img src={quotes} alt="double_quotes" className="w-[42.6px] h-[27.6px] object-contain" />
     <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-white my-10">
       {content}
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -13,7 +13,7 @@ const Testimonials = () => (
      
     </div>
 
-    <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
+    <div className="flex flex-wrap  sm:justify-start justify-center w-full feedback-container relative z-[1]">
       {feedback.map((card) => <FeedbackCard key={card.id} {...card} />)}
     </div>
   </section>
